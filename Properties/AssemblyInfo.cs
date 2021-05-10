f08549c995e76b233487a09dc9709f8f28967702
johnjore <john@jore.no>
2021-05-10 21:00:03 +1000

    Initial setup

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("hajk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Hiking with (free) Offline Maps")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("hajk")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
