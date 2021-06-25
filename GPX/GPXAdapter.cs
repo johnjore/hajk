dd25e3279fd318185b287f868c762c151c468a56
johnjore <john@jore.no>
2021-06-25 18:30:13 +1000

    Misc

--- GPX/GPXAdapter.cs
+++ GPX/GPXAdapter.cs
@@ -19,7 +19,7 @@ using Mapsui.Utilities;
 using hajk.Data;
 using hajk.Fragments;
 using Serilog;
-
+using Google.Android.Material.Navigation;
 
 namespace hajk.Adapter
 {
@@ -64,19 +64,46 @@ namespace hajk.Adapter
                         case Resource.Id.gpx_menu_followroute:
                             Log.Information($"Follow route '{vh.Name.Text}'");
 
+                            //Get the route
                             var route  = RouteDatabase.GetRouteAsync(vh.Id).Result;
 
+                            //Add GPX to Map
                             ILayer lineStringLayer = Import.CreateRouteLayer(route.WayPoints, Import.CreateRouteStyle());
                             lineStringLayer.IsMapInfoLayer = true;
                             lineStringLayer.Enabled = true;
                             Fragment_map.map.Layers.Add(lineStringLayer);
 
+                            //Start recording
+                            RecordTrack.StartTrackTimer();
+                            NavigationView nav = MainActivity.mContext.FindViewById<NavigationView>(Resource.Id.nav_view);
+                            var item = nav.Menu.FindItem(Resource.Id.nav_recordtrack);
+                            item.SetTitle("Stop Recording");
+
+                            /**/
+                            //var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(location.Longitude, location.Latitude);
+                            //Fragment_map.mapControl.Navigator.CenterOn(sphericalMercatorCoordinate);
+
+                            //Fragment_map.mapControl.
+
+                            //Switch to map
                             MainActivity.SwitchFragment("Fragment_map", (FragmentActivity)parent.Context);
 
                             break;
                         case Resource.Id.gpx_menu_showonmap:
                             Log.Information($"Show route on map '{vh.Name.Text}'");
-                            Toast.MakeText(parent.Context, "show on map  " + vh.AdapterPosition.ToString(), ToastLength.Short).Show();
+
+                            //Get the route
+                            var route_1 = RouteDatabase.GetRouteAsync(vh.Id).Result;
+
+                            //Add GPX to Map
+                            ILayer lineStringLayer_1 = Import.CreateRouteLayer(route_1.WayPoints, Import.CreateRouteStyle());
+                            lineStringLayer_1.IsMapInfoLayer = true;
+                            lineStringLayer_1.Enabled = true;
+                            Fragment_map.map.Layers.Add(lineStringLayer_1);
+
+                            //Switch to map
+                            MainActivity.SwitchFragment("Fragment_map", (FragmentActivity)parent.Context);
+
                             break;
                         case Resource.Id.gpx_menu_deleteroute:
                             Log.Information($"Delete route '{vh.Name.Text}'");
