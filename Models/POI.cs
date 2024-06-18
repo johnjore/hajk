e8e3f1efce9a7c983f7ec06a95950af780e6887b
johnjore <john@jore.no>
2024-06-18 20:12:00 +1000

    Misc warnings

--- Models/POI.cs
+++ Models/POI.cs
@@ -6,9 +6,9 @@ namespace hajk.Models
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Symbol { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Symbol { get; set; }
         public decimal Lat { get; set; }
         public decimal Lon { get; set; }
     }
--- Models/Route.cs
+++ Models/Route.cs
@@ -7,13 +7,13 @@ namespace hajk.Models
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public GPXType GPXType{ get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public float Distance { get; set; }
         public int Ascent { get; set; }
         public int Descent { get; set; }
-        public string Description { get; set; }
-        public string GPX { get; set; }
-        public string ImageBase64String { get; set; }
+        public string? Description { get; set; }
+        public string? GPX { get; set; }
+        public string? ImageBase64String { get; set; }
     }
 
     public enum GPXType : uint
