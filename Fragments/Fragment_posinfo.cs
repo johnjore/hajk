3a7915fe5a4447ddd3e1fdcd84029a34d6d1fe3e
John Jore <john@jore.no>
2025-07-21 17:32:05 +1000

    Remove Microcharts

--- Fragments/Fragment_posinfo.cs
+++ Fragments/Fragment_posinfo.cs
@@ -1,50 +1,22 @@
-﻿using Android.Content;
-using Android.Content.Res;
-using Android.Graphics;
-using Android.Media;
-using Android.Net.Vcn;
+﻿
 using Android.OS;
-using Android.Renderscripts;
-using Android.Runtime;
-using Android.Util;
 using Android.Views;
 using Android.Widget;
-using AndroidX.AppCompat.App;
-using AndroidX.AppCompat.Widget;
-using AndroidX.Core.View;
-using AndroidX.DrawerLayout.Widget;
 using AndroidX.Fragment;
 using AndroidX.Fragment.App;
-using GeoTiffCOG;
 using GPXUtils;
-using hajk.Adapter;
-using hajk.Data;
-using hajk.Fragments;
-using Java.Nio.FileNio.Attributes;
-using Mapsui.Projections;
-using Microcharts;
-using Microcharts.Droid;
-using Microsoft.Maui.ApplicationModel;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Storage;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
 using OxyPlot.Xamarin.Android;
 using Serilog;
-using SharpCompress.Compressors.RLE90;
 using SharpGPX;
-using SharpGPX.GPX1_0;
 using SharpGPX.GPX1_1;
-using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Reflection;
-using System.Reflection.Metadata.Ecma335;
-using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -266,116 +238,7 @@ namespace hajk.Fragments
 
             return null;
         }
-
-        private void ConfigureGraph(Android.Views.View view, Android.Locations.Location GpsLocation, GPXUtils.Position MapPosition)
-        {
-            try
-            {
-                var chartView = view.FindViewById<ChartView>(Resource.Id.chartElevation);
-                chartView.Visibility = ViewStates.Gone;
-
-                if (MainActivity.ActiveRoute == null)
-                    return;
-
-                var route = MainActivity.ActiveRoute.Routes.First();
-                if (route == null)
-                    return;
-
-                if (route.rtept.Count == 0)
-                    return;
-
-                /**/
-                //MapPoint closest to Route. Distance should be 0... Can't we find this quicker by looking for LatLng in the route?
-                var r2 = MapInformation.FindClosestWayPoint(route, new GPXUtils.Position(MapPosition.Latitude, MapPosition.Longitude, 0, false, null));
-                var map_index = r2.Item2;
-
-                //WayPoint we are closest to
-                var r1 = MapInformation.FindClosestWayPoint(route, new GPXUtils.Position(GpsLocation.Latitude, GpsLocation.Longitude, 0, false, null));
-                var gps_index = r1.Item2;
-
-                List<ChartEntry> entries = new List<ChartEntry>();
-
-                //Entries
-                SKColor Color = SKColor.Parse("#00ff00"); //Green
-                for (int i = 0; i < route.rtept.Count; i++)
-                {
-                    var entry = new ChartEntry((float)route.rtept[i].ele)
-                    {
-                        Color = Color,
-                    };
-
-                    //If GPS Position, change to Red and add label
-                    if (i == gps_index)
-                    {
-                        Color = SKColor.Parse("#ff0000"); //Red
-                        entry.Color = Color;
-                        entry.Label = "G";
-                        entry.TextColor = SKColor.Parse("#000000"); //Black
-                    }
-
-                    //If Map Position, add label
-                    if (i == map_index)
-                    {
-                        Color = SKColor.Parse("#800080"); //Purple
-                        entry.Color = Color;
-                        entry.Label = "M";
-                        entry.TextColor = SKColor.Parse("#000000"); //Black
-                    }
-
-                    //Start
-                    if (i == 0)
-                    {
-                        entry.Label = "0";
-                        entry.TextColor = SKColor.Parse("#000000"); //Black
-                    }
-
-                    //End
-                    if (i == route.rtept.Count - 1)
-                    {
-                        var t = Import.ParseGPXtoRoute(route);
-                        entry.Label = (t.Item2 / 1000).ToString("N1");
-                        entry.TextColor = SKColor.Parse("#000000"); //Purple
-                    }
-
-                    entries.Add(entry);
-                }
-
-                //Chart configuration
-                const string text = "0";
-                var typeface = SKFontManager.Default.MatchCharacter(text[0]);
-                var a = SKFontManager.Default;
-
-                var chart = new LineChart
-                {
-                    LineMode = LineMode.Straight,
-                    LineSize = 5,
-                    PointMode = PointMode.None,
-                    AnimationDuration = TimeSpan.FromSeconds(0),
-                    LabelOrientation = Microcharts.Orientation.Vertical, //Change to Horizontal when support for X axis is done
-                    LabelTextSize = 40.0f,
-                    LabelColor = SKColor.Parse("#000000"),
-                    Margin = 10,
-                    ShowYAxisLines = true,
-                    ShowYAxisText = true,
-                    YAxisTextPaint = new SKPaint()
-                    {
-                        Typeface = SKTypeface.Default,
-                        TextSize = 32.0f,
-                        TextAlign = SKTextAlign.Left,
-                    },
-                    Entries = entries
-                };
-
-                //Set the Chart
-                chartView.Chart = chart;
-                chartView.Visibility = ViewStates.Visible;
-            }
-            catch (Exception ex)
-            {
-                Serilog.Log.Fatal(ex, "posinfo - Crashed while creating elevation graph");
-            }
-        }
-
+        
         private (LineSeries? lineSeries, double MinX, double MaxX, double distance) CreateSeries(wptTypeCollection? waypoints, string? hexcolor, double distance_m, int start, int? end)
         {
             if (waypoints == null || waypoints?.Count < end)
